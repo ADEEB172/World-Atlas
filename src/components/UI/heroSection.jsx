90947175085d55c333c564d160aacfd2332162c7
Mohd Adeeb <adeeb3873@gmail.com>
2026-07-19 01:27:47 +0530

    dd

--- src/components/UI/heroSection.jsx
+++ src/components/UI/heroSection.jsx
@@ -1,6 +1,12 @@
 import { FaLongArrowAltRight } from "react-icons/fa";
+import { Country } from "../../pages/Country";
 
 export const HeroSection = () => {
+
+  const startExploring = () => {
+    <Country/>
+  }
+
   return (
     <main className="hero-section main">
       <div className="container hero-panel">
@@ -14,7 +20,7 @@ export const HeroSection = () => {
             Discover the world with our travel website, where you can explore new
             destinations, plan your next adventure, and connect with fellow travelers.
           </p>
-          <button className="hero-button">
+          <button className="hero-button" onClick={startExploring}>
             Start Exploring <FaLongArrowAltRight />
           </button>
         </div>
